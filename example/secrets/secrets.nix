0c12215fc1602a944a6da0fde401838ea6508aee
Sridhar Ratnakumar <srid@srid.ca>
2024-06-22 18:38:19 -0400

    reintroduce

--- example/secrets/secrets.nix
+++ example/secrets/secrets.nix
@@ -1,1 +1,9 @@
-{ }
+let
+  srid = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
+  users = [ srid ];
+
+  systems = [ ];
+in
+{
+  "github-nix-ci/srid.token.age".publicKeys = users ++ systems;
+}
