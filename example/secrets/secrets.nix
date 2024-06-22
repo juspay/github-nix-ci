7a5cc8041a399d8dd458b03a5672d7e23c729a91
Sridhar Ratnakumar <srid@srid.ca>
2024-06-22 18:40:27 -0400

    Revert "reintroduce"
    This reverts commit 0c12215fc1602a944a6da0fde401838ea6508aee.

--- example/secrets/secrets.nix
+++ example/secrets/secrets.nix
@@ -1,9 +1,1 @@
-let
-  srid = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
-  users = [ srid ];
-
-  systems = [ ];
-in
-{
-  "github-nix-ci/srid.token.age".publicKeys = users ++ systems;
-}
+{ }
