2a3538e1dc93260756c7b400a3b596f0ec33f646
Sridhar Ratnakumar <srid@srid.ca>
2024-06-22 18:42:31 -0400

    add secrets anyway for example

--- example/secrets/secrets.nix
+++ example/secrets/secrets.nix
@@ -1,1 +1,11 @@
-{ }
+let
+  srid = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
+  users = [ srid ];
+
+  # Normally you would add the host's pub keys here.
+  systems = [ ];
+in
+{
+  "github-nix-ci/srid.token.age".publicKeys = users ++ systems;
+  "github-nix-ci/zed-industries.token.age".publicKeys = users ++ systems;
+}
